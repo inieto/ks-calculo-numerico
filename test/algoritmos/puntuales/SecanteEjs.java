8a1d58e59600f3e182eeed2295bb4e8a4656ab49
nietofarias <nietofarias@8da13c3f-0af9-92fa-2d42-2bac6a93814e>
2012-11-07 03:30:48 +0000

    Algoritmo de la Secante + Validaciones faltantes

---
+++ test/algoritmos/puntuales/SecanteEjs.java
@@ -0,0 +1,35 @@
+package algoritmos.puntuales;
+
+import static org.junit.Assert.*;
+import static java.lang.Math.*;
+import interfaces.Funcion;
+
+import org.junit.Test;
+
+public class SecanteEjs {
+	private double tolerancia = 0.001;
+
+	@Test
+	public void ejercicio1() {
+		Secante algoritmo = new Secante();
+		algoritmo.setTolerancia(tolerancia);
+		algoritmo.setAproxInicial0(2);
+		algoritmo.setAproxInicial1(-1);
+		//Lo probé con (X0,X1): (0,PI/4), (0,2*PI), (PI/4 , 0.7395361335152383), (-1,2), (2,-1) y anda con todos!
+		//0.7395361335152383 era la segunda aproximación del ej de la Tangente
+		algoritmo.setFuncionEjercicio(new Funcion() {
+			@Override
+			public double calcular(double X) {
+				double resultado = cos(X)-X;
+				System.out.println("--> f("+X+") = cos("+X+") - "+X+" = "+resultado);
+				return resultado;
+			}
+			@Override
+			public String desc() {
+				return "f(x)=cos(x)-x, PI/4<=X<=PI/2";
+			}
+		});
+		double raiz = algoritmo.calcular();
+		assertEquals(0.73908513, raiz, tolerancia);
+	}
+}
