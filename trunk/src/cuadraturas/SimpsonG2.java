bfc261c21b9d5dd42fafc5b3e6f7edef3b035965
nietofarias <nietofarias@8da13c3f-0af9-92fa-2d42-2bac6a93814e>
2012-11-08 18:30:24 +0000

    Listo Trapecio Arreglo comments de Simpson

--- trunk/src/cuadraturas/SimpsonG2.java
+++ trunk/src/cuadraturas/SimpsonG2.java
@@ -9,8 +9,7 @@ import excepciones.Incompleto;
 
 /** <b>Polinomio de Grado 2 de Simpson</b>
 	Aproxima de VALORES de Integrales (usando el polinomio interpolador de lagrange). Valores y no fórmulas 
-	Es más preciso que Trapecio porque usa 3 pares [x,f(x)]
-	de ejecutar este algoritmo.
+	Es más preciso que Trapecio porque usa un polinomio de 3 pares [x,f(x)]
 	 
 	La idea es: hay una función f(x) que no se le puede sacar la integral (definida en un intervalo)
 	Pero se pueden aproximar su integral usando este algoritmo.
@@ -18,7 +17,6 @@ import excepciones.Incompleto;
 	La CAGADA con este método es que como hay que reemplazar y e integrar a mano los polinomios 
 	de lagrange no se puede hacer un algoritmo genérico para un vector x cualquiera.
 	Hay que hacer un algoritmo por cada grado del polinomio (y cada vector x).
-	Este sirve para un conjunto de 3 valores.
 */
 public class SimpsonG2 {
 	private Double limiteMin;
@@ -46,10 +44,10 @@ public class SimpsonG2 {
 		System.out.println("f(X): "+Arrays.toString(f));
 		System.out.println("Se calculará la integral aproximada en el intervalo X por Simpson");
 		double h = abs(x[0] - x[1]);
-		System.out.println("I(f) en ["+x[0]+","+x[2]+"] = (h/3) * ( f(x0) + 4.f(x1) + f(x2) )");
-		System.out.println("I(f) en ["+x[0]+","+x[2]+"] = ("+h+"/3) * ( "+f[0]+" + 4."+f[1]+" + "+f[2]+" )");
+		System.out.println("I2(f) en ["+x[0]+","+x[2]+"] = (h/3) * ( f(x0) + 4.f(x1) + f(x2) )");
+		System.out.println("I2(f) en ["+x[0]+","+x[2]+"] = ("+h+"/3) * ( "+f[0]+" + 4."+f[1]+" + "+f[2]+" )");
 		double resultado = (h/3) * ( f[0] + 4 * f[1] + f[2] );
-		System.out.println("I(f) en ["+x[0]+","+x[2]+"] = " + resultado);
+		System.out.println("I2(f) en ["+x[0]+","+x[2]+"] = " + resultado);
 		return resultado;
 	}
 	
--- trunk/test/cuadraturas/TrapecioEjs.java
+++ trunk/test/cuadraturas/TrapecioEjs.java
@@ -5,16 +5,16 @@ import interfaces.Funcion;
 import org.junit.Test;
 import static java.lang.Math.*;
 
-public class SimpsonEjs {
+public class TrapecioEjs {
 
 	@Test
 	public void ejercicio1() {
 		double x[] = {0, 1};
 		double d = 2.5;
-		SimpsonG2 sG2 = new SimpsonG2();
-		sG2.setLimiteMin(x[0]);
-		sG2.setLimiteMax(x[1]);
-		sG2.setFuncionEjercicio(new Funcion() {
+		TrapecioG1 tG1 = new TrapecioG1();
+		tG1.setLimiteMin(x[0]);
+		tG1.setLimiteMax(x[1]);
+		tG1.setFuncionEjercicio(new Funcion() {
 			
 			@Override
 			public String desc() {
@@ -27,8 +27,8 @@ public class SimpsonEjs {
 				return pow( E, -x*x/2 ) / sqrt(2*PI);
 			}
 		});
-		double result = sG2.calcular();
-		System.out.println("Valor aproximado de I2(f): " + result);
+		double result = tG1.calcular();
+		System.out.println("Valor aproximado de I1(f): " + result);
 	}
 	
 }
