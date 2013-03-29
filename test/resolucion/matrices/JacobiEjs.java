bb7767273a1558a3d2e5c32d9c86e533bddd12d8
nietofarias <nietofarias@8da13c3f-0af9-92fa-2d42-2bac6a93814e>
2013-03-29 16:20:53 +0000

    cierre a la materia

--- test/resolucion/matrices/JacobiEjs.java
+++ test/resolucion/matrices/JacobiEjs.java
@@ -27,9 +27,39 @@ public class JacobiEjs {
 		jacob.setLimiteIntentos(this.limiteIntentos);
 //		{0,0,0} diverge. {-0.9,2,2} llega a la raiz!. Todas las otras combinaciones con los números 2 divergen.
 		jacob.setAproximacionInicial(new double[]{-0.9,2,2});
+//		jacob.setAproximacionInicial(new double[]{0,0,0});
 		double[] x = jacob.calcular();
-		imprimirMatriz(x);
+		System.out.println("Solucion final "+ Arrays.toString(x));
 		assertTrue(Arrays.equals(x, new double[]{-1,2,2}));	//{-1,2,2} es raiz hallada con EliminacionGauseana
+	}
+	
+	@Test
+	public void eJERCICIO3() {
+		double[][] a = {{ 4,-1, 1 },
+				{ -1, 4.25, 2.75 },
+				{ 1, 2.75, 3.5 }};
+		double[] b = {1,1,1};
+		Jacobi jacob = new Jacobi();
+		jacob.setMatriz(a, b);
+		jacob.setTolerancia(this.tolerancia);
+		jacob.setLimiteIntentos(this.limiteIntentos);
+//		{0,0,0} diverge. {-0.9,2,2} llega a la raiz!. Todas las otras combinaciones con los números 2 divergen.
+		jacob.setAproximacionInicial(new double[]{0.4, 0.4, -0.1});
+//		jacob.setAproximacionInicial(new double[]{0,0,0});
+		double[] x = jacob.calcular();
+		System.out.println("Solucion final "+ Arrays.toString(x));
+		assertTrue(Arrays.equals(x, new double[]{-1,2,2}));	//{-1,2,2} es raiz hallada con EliminacionGauseana
+	}
+	
+	public void e2JERCICIO3() {
+		double[][] a = {{ 4,-1, 1 },
+						{ -1, 4.25, 2.75 },
+						{ 1, 2.75, 3.5 }};
+		double[] b = {1,1,1};
+		EliminacionGauseana eg = new EliminacionGauseana();
+		eg.setMatriz(a, b);
+		double[] x = eg.calcular();
+		imprimirMatriz(x);
 	}
 	
 	private void imprimirMatriz(double[] x) {
